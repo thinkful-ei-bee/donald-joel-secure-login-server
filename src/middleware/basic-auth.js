665c9a3d9fd4b69a2eadc9a5dcbbeb795c93eba8
joelcorey <joelcorey@fastmail.com>
2019-04-17 08:38:42 -0700

    init - transfer

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcryptjs');
+const AuthService = require('../auth/auth-service');
+
+function requireAuth(req, res, next) {
+  // console.log('requireAuth')
+  // console.log(req.get('Authorization'))
+
+  const authToken = req.get('Authorization') || '';
+
+  let basicToken;
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'Missing basic token' });
+  } else {
+    basicToken = authToken.slice('basic '.length, authToken.length);
+  }
+
+  const [tokenUserName, tokenPassword] = Buffer
+    .from(basicToken, 'base64')
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  AuthService.getUserWithUserName(
+    req.app.get('db'),
+    tokenUserName
+  ).then(user => {
+    if(!user) {
+      return res.status(401).json({ error: 'Unauthorized request' });
+    }
+
+    return bcrypt.compare(tokenPassword, user.password)
+      .then(passwordsMatch => {
+        if (!passwordsMatch) {
+          return res.status(401).json({ error: 'Unauthorized request' });
+        }
+
+        req.user = user;
+        next();
+      });
+  })
+    .catch(next);
+}
+
+module.exports = {
+  requireAuth,
+};
